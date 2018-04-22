1f4ce9cbc9ed0eab5c238da23d4a8353073d2f47
Szymek <matszym325@gmai.com>
2018-04-22 14:25:58 +0200

    working base blob

--- Web/ClientApp/app/file-downloader.service.ts
+++ Web/ClientApp/app/file-downloader.service.ts
@@ -1,6 +1,6 @@
+
 import { Injectable, Inject } from "@angular/core";
-import { HttpClient, HttpParams, } from "@angular/common/http"
-import { Observable } from 'rxjs/Observable';
+import { HttpClient, HttpParams } from "@angular/common/http";
 
 
 @Injectable()
@@ -8,24 +8,39 @@ export class FileDownloaderService {
 
     constructor(private httpClient: HttpClient, @Inject("BASE_URL") private baseUrl: string) {}
 
-    //getFile(fileName: string) {
-
-    //    //const params = new HttpParams().set("fileName", fileName);
-    //    //this.httpClient.get(this.baseUrl + "getFile", { responseType: "application/octet-stream", params:params})
-    //    //    .subscribe(
-    //    //        response => {
-    //    //            console.log(response);
-    //    //        },
-    //    //        error => { console.log(error) });
-    //}
-
-    getFile(fileName: string): Observable<Blob> {
+    getFile(fileName: string) {
+        let objectUrl: string = null;
         const params = new HttpParams().set("fileName", fileName);
-        return this.httpClient
-            .get(this.baseUrl + "getFile", {
-                responseType: "blob",
-                params:params
-            });
+        this.httpClient.get(this.baseUrl + "getFile", { responseType: "blob", params: params })
+            .subscribe(
+                m => {
+                    objectUrl = URL.createObjectURL(m);
+                    console.log(objectUrl);
+                },
+                error => { console.log(error) });
     }
 
+    //getFile(fileName: string): Observable<Blob> {
+    //    const params = new HttpParams().set("fileName", fileName);
+    //    return this.httpClient
+    //        .get(this.baseUrl + "getFile", {
+    //            responseType: "blob",
+    //            params:params
+    //        });
+    //}
+
+    //getFile(fileName: string): Observable<Blob>  {
+    //    const params = new HttpParams().set("fileName", fileName);
+    //    let objectUrl: string = null;
+
+    //    this.httpClient
+    //        .get(this.baseUrl + "getFile",
+    //            {
+    //                responseType: 'blob',
+    //                params: params
+    //            })
+    //        .subscribe(m => {
+    //            objectUrl = URL.createObjectURL(m);
+    //        });
+    //}
 }
