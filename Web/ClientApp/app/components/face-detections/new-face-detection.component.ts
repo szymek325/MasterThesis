547d6837708f1b02a30244715a7ed159921aa59f
Szymek <matszym325@gmai.com>
2018-04-29 15:35:35 +0200

    fix

--- Web/ClientApp/app/components/face-detections/new-face-detection.component.ts
+++ Web/ClientApp/app/components/face-detections/new-face-detection.component.ts
@@ -2,18 +2,39 @@ import { Component, OnInit } from "@angular/core";
 import {FaceDetectionService} from "../../services/face-detection.service";
 import {AlertService} from "../../services/alert.service";
 
+var inputElement: HTMLInputElement;
 
 @Component({
     selector: "new-face-detection",
     templateUrl: "./new-face-detection.component.html",
+    styleUrls: ["./face-detection.components.css"],
 })
 export class NewFaceDetectionComponent implements OnInit {
     name: string;
-    files:any;
-    constructor(private requestDownloader: FaceDetectionService, private alertService: AlertService) {}
+    files: any;
+    formData: any;
+
+    constructor(private requestDownloader: FaceDetectionService, private alertService: AlertService) { }
+
+    add(event: Event) {
+        inputElement = ((event.srcElement || event.target) as HTMLInputElement);
+        this.files = inputElement.files;
+
+        if (!this.files) return;
+
+        this.formData = new FormData();
+        for (let index = 0; index < this.files.length; index++) {
+            const file = this.files[index];
+            this.formData.set(file.name, file);
+        }
+
+        console.log(this.formData);
+    }
 
     ngOnInit() {
 
     }
+
+
 
 }
