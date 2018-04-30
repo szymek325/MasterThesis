630eb6c32ec6653405713464803bc7cd03a59065
szymek325 <matszym325@gmai.com>
2018-04-30 13:49:58 +0200

    update db example

--- database_connection_tester.py
+++ database_connection_tester.py
@@ -1,3 +1,4 @@
+from entities.face_detection import FaceDetection
 from entities.sensors_reading import SensorsReading
 from helpers.database_connection import Base, engine, Session
 
@@ -12,9 +13,11 @@ session = Session()
 # session.close()
 
 # read
-readings = session.query(SensorsReading).all()
+readings = session.query(FaceDetection).all()
 print('\n### All readings:')
 for reading in readings:
-    print(f"{reading.id} {reading.humidity} {reading.temperature}")
+    print(f"{reading.id} {reading.name} {reading.statusId}")
+    reading.statusId = 2
+session.commit()
 print('dupa')
 session.close()
