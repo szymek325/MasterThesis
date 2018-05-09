e63d92a4d35471d516fec7bc3024b250cc7f6845
szymek325 <matszym325@gmai.com>
2018-05-09 08:17:12 +0200

    hosted db

--- DataLayer/ServiceCollectionExtension.cs
+++ DataLayer/ServiceCollectionExtension.cs
@@ -10,13 +10,13 @@ namespace DataLayer
     {
         public static IServiceCollection AddDataLayerModule(this IServiceCollection services)
         {
-            services.AddDbContext<MasterContext>(options => options.UseSqlServer(
-                    "Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=TestDb;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;",
-                    optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(MasterContext).GetTypeInfo().Assembly.GetName().Name)));
             //services.AddDbContext<MasterContext>(options => options.UseSqlServer(
-            //    "Data Source=den1.mssql6.gear.host;Initial Catalog=masterthesisdb;Integrated Security=False;User ID=masterthesisdb;Password=Zp9P?Q!ezuXH;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False",
-            //    optionsBuilder =>
-            //        optionsBuilder.MigrationsAssembly(typeof(MasterContext).GetTypeInfo().Assembly.GetName().Name)));
+            //        "Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=TestDb;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;",
+            //        optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(MasterContext).GetTypeInfo().Assembly.GetName().Name)));
+            services.AddDbContext<MasterContext>(options => options.UseSqlServer(
+                "Data Source=den1.mssql6.gear.host;Initial Catalog=masterthesisdb;Integrated Security=False;User ID=masterthesisdb;Password=Zp9P?Q!ezuXH;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False",
+                optionsBuilder =>
+                    optionsBuilder.MigrationsAssembly(typeof(MasterContext).GetTypeInfo().Assembly.GetName().Name)));
             services.AddTransient<IFaceDetectionRepository, FaceDetectionRepository>();
             services.AddTransient<ISensorsReadingRepository, SensorsReadingRepository>();
             services.AddTransient<IFileRepository, FileRepository>();
