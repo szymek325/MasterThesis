f6a7d9ea3c9660db2119078057eb57c79e4bac3d
Mateusz Szymkowiak <mateusz.x.szymkowiak@gsk.com>
2018-05-10 15:22:24 +0200

    ctor

--- Domain/FaceRecognition/FaceRecognitionService.cs
+++ Domain/FaceRecognition/FaceRecognitionService.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
+using DataLayer.Repositories.Interface;
+using Domain.Configuration;
 using Domain.FaceRecognition.DTO;
+using Microsoft.Extensions.Logging;
 
 namespace Domain.FaceRecognition
 {
     public class FaceRecognitionService : IFaceRecognitionService
     {
+        private readonly IFaceRecognitionRepository faceRecoRepo;
+        private readonly IMapper mapper;
+        private readonly IGuidProvider guid;
+        private readonly ILogger<FaceRecognitionService> logger;
+
+        public FaceRecognitionService(IFaceRecognitionRepository faceRecoRepo, IMapper mapper, IGuidProvider guid,
+            ILogger<FaceRecognitionService> logger)
+        {
+            this.faceRecoRepo = faceRecoRepo;
+            this.mapper = mapper;
+            this.guid = guid;
+            this.logger = logger;
+        }
+
         public Task<IEnumerable<FaceRecoRequest>> GetAllFaceDetectionsAsync()
         {
             throw new NotImplementedException();
