6e927a477a5bea71e169d3e19fb9c6378c7db269
Mateusz Szymkowiak <mateusz.x.szymkowiak@gsk.com>
2018-05-22 16:05:38 +0200

    small update

--- domain/people_manager.py
+++ domain/people_manager.py
@@ -1,4 +1,8 @@
+import os
+
+from configuration_global.config_reader import ConfigReader
 from configuration_global.logger_factory import LoggerFactory
+from dataLayer.repositories.person_repository import PersonRepository
 from domain.directory_manager import DirectoryManager
 
 
@@ -6,11 +10,15 @@ class PeopleManager():
     def __init__(self):
         self.logger = LoggerFactory()
         self.directoryManager = DirectoryManager()
+        self.config= ConfigReader()
+        self.peopleRepo= PersonRepository()
 
     def check_if_people_exists_locally(self, ids: [int]):
         for person_id in ids:
-            self.logger.info(person_id)
+            if not os.path.exists(f"{self.config.people_path}"/person_id):
+                self.download_person()
         self.logger.info("check_if_people_exists_locally")
 
     def download_person(self):
-        self.logger.info("download_person")
+        person
+
