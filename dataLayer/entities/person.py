287fe1277b267817ffcc26f1b1ece74617ecde83
szymek325 <matszym325@gmail.com>
2018-07-20 10:16:42 +0200

    images added to repo as they should, but first  result should be created

--- dataLayer/entities/person.py
+++ dataLayer/entities/person.py
@@ -2,7 +2,6 @@ from sqlalchemy import Column, String, Integer, Date
 from sqlalchemy.orm import relationship
 
 from dataLayer.database_connection import Base
-from dataLayer.entities.image_attachment import ImageAttachment
 
 
 class Person(Base):
@@ -10,4 +9,4 @@ class Person(Base):
 
     id = Column('Id', Integer, primary_key=True)
     name = Column('Name', String)
-    images = relationship(ImageAttachment)
+    images = relationship("ImageAttachment")
