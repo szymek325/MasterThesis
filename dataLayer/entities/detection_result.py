387a4ac948546aa7903b99a41d7cf3a4f7a8f257
szymek325 <matszym325@gmail.com>
2018-07-20 11:27:51 +0200

    numpy to normal int, uploading result works!

--- dataLayer/entities/detection_result.py
+++ dataLayer/entities/detection_result.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, String, Integer, Date, ForeignKey
 from sqlalchemy.orm import relationship
 
 from dataLayer.database_connection import Base
+import numpy as np
 
 
 class DetectionResult(Base):
@@ -15,10 +16,12 @@ class DetectionResult(Base):
     detection_id = Column("DetectionId", Integer, ForeignKey("Detection.Id"), nullable=True)
     detection = relationship("Detection")
     detection_type_id = Column("DetectionTypeId", Integer)
-    image = relationship("ImageAttachment",uselist=False)
+    image = relationship("ImageAttachment", uselist=False)
 
     def __init__(self, coordinates, request_id, detection_type_id, image_attachment):
-        self.startX, self.startY, self.endX, self.endY = coordinates
+        startX, startY, endX, endY = coordinates
+        self.startX, self.startY, self.endX, self.endY = np.asscalar(startX), np.asscalar(startY), np.asscalar(
+            endX), np.asscalar(endY)
         self.detection_id = request_id
         self.detection_type_id = detection_type_id
         self.image = image_attachment
