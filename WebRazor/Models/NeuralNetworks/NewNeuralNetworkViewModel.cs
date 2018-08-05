46f06777af47f79ac65394c5cde724db42fcfa9d
szymek325 <matszym325@gmai.com>
2018-08-05 13:25:52 +0200

    People Validator

--- WebRazor/Models/NeuralNetworks/NewNeuralNetworkViewModel.cs
+++ WebRazor/Models/NeuralNetworks/NewNeuralNetworkViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Domain.People.DTO;
+using WebRazor.Validators;
 
 namespace WebRazor.Models.NeuralNetworks
 {
@@ -9,6 +10,7 @@ namespace WebRazor.Models.NeuralNetworks
         [Required]
         public string Name { get; set; }
         [Required]
+        [ValidatePeople(ErrorMessage = "You have to pick at leat 2 people")]
         public List<PersonAsCheckbox> PeopleCheckboxes { get; set; }
     }
 }
