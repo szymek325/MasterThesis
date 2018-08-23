fa05bb106737b00d5b80e001db64d80a8f8cd026
Mateusz Szymkowiak <mateusz.x.szymkowiak@gsk.com>
2018-08-23 13:52:29 +0200

    readings views

--- Domain/SensorsReading/ReadingsProvider.cs
+++ Domain/SensorsReading/ReadingsProvider.cs
@@ -35,14 +35,14 @@ namespace Domain.SensorsReading
         {
             var distinctDates = readingsRepo.GetAll().Select(x =>new DateOutput{
                 Date = x.CreationTime.Date}
-            ).Distinct().ToList();
+            ).Distinct().OrderByDescending(x=>x.Date).ToList();
             return distinctDates;
         }
 
         public IEnumerable<Reading> GetReadingsFromDay(string day)
         {
             var dateTimeDay = DateTime.Parse(day);
-            var sensorReadings = readingsRepo.GetByDay(dateTimeDay);
+            var sensorReadings = readingsRepo.GetByDay(dateTimeDay).OrderByDescending(x=>x.CreationTime);
             var readings = mapper.Map<IEnumerable<Reading>>(sensorReadings);
             return readings;
         }
---
+++ WebRazor/Controllers/ReadingsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.SensorsReading;
+using Domain.SensorsReading.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebRazor.Controllers
+{
+    public class ReadingsController : Controller
+    {
+        private readonly ILogger<ReadingsController> logger;
+        private readonly IReadingsProvider readingsProvider;
+
+        public ReadingsController(ILogger<ReadingsController> logger, IReadingsProvider readingsProvider)
+        {
+            this.logger = logger;
+            this.readingsProvider = readingsProvider;
+        }
+
+        public IEnumerable<Reading> GetAll()
+        {
+            return readingsProvider.GetAllReadings();
+        }
+
+        public IActionResult AllDates()
+        {
+            var dates=readingsProvider.GetDistinctDates();
+            return View(dates);
+
+        }
+
+        public IActionResult DatePerDay(string day)
+        {
+            var readings=readingsProvider.GetReadingsFromDay(day);
+            return View(readings);
+        }
+    }
+}
