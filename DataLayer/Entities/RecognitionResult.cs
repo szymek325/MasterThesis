347c0f9bfaeef0d8f36bb401e9444937f625107a
szymek325 <matszym325@gmai.com>
2018-09-02 18:24:03 +0200

    confidence as float

--- DataLayer/Entities/RecognitionResult.cs
+++ DataLayer/Entities/RecognitionResult.cs
@@ -6,7 +6,7 @@ namespace DataLayer.Entities
     public class RecognitionResult : EntityBase
     {
         public int IdentifiedPersonId { get; set; }
-        public int Confidence { get; set; }
+        public float Confidence { get; set; }
         public int NeuralNetworkFileId { get; set; }
         public NeuralNetworkFile NeuralNetworkFile { get; set; }
         public int RecognitionId { get; set; }
