449720371f024fbce8d345875e84fa5c1d615a43
szymek325 <matszym325@gmai.com>
2018-09-11 20:50:24 +0200

    tester ready

--- cognitive_face_client/azure_face_recognizer.py
+++ cognitive_face_client/azure_face_recognizer.py
@@ -24,7 +24,7 @@ class AzureFaceRecognizer():
     def recognize_face_without_db(self, neural_network_id, image_file_path):
         azure_file = self.neuralNetworkFilesRepo.get_azure_file_connected_to_neural_network(neural_network_id)
         if azure_file is not null and azure_file is not None:
-            self.__get_result_from_azure__(azure_file, image_file_path)
+            return self.__get_result_from_azure_without_db__(azure_file, image_file_path)
 
     def __get_result_from_azure_without_db__(self, azure_file, image_file_path):
         start_time = time.time()
--- domain/face_recognition/face_recognizer_provider.py
+++ domain/face_recognition/face_recognizer_provider.py
@@ -2,6 +2,7 @@ import os
 
 from configuration_global.paths_provider import PathsProvider
 from dataLayer.repositories.neural_network_file_repository import NeuralNetworkFileRepository
+from dataLayer.type_providers.neural_network_types import NeuralNetworkTypes
 from opencv_client.neural_network.neural_network_provider import NeuralNetworkProvider
 
 
@@ -19,3 +20,19 @@ class FaceRecognizerProvider():
             recognizer = self.neuralNetworkProvider.create_neural_network_by_type_id(nn_path, file.neuralNetworkTypeId)
             face_recognizers.append([recognizer, file.id])
         return face_recognizers
+
+    def create_open_cv_face_recognizers_with_type(self, request_id):
+        nn_files = self.neuralNetworkFilesRepo.get_all_open_cv_files_connected_to_neural_network(request_id)
+        eigen = []
+        fisher = []
+        lbph = []
+        for file in nn_files:
+            nn_path = os.path.join(self.pathsProvider.local_neural_network_path(), str(file.neuralNetworkId), file.name)
+            recognizer = self.neuralNetworkProvider.create_neural_network_by_type_id(nn_path, file.neuralNetworkTypeId)
+            if file.neuralNetworkTypeId is NeuralNetworkTypes().fisher_id:
+                fisher = recognizer
+            elif file.neuralNetworkTypeId is NeuralNetworkTypes().eigen_id:
+                eigen = recognizer
+            elif file.neuralNetworkTypeId is NeuralNetworkTypes().lbph_id:
+                lbph = recognizer
+        return fisher, eigen, lbph
